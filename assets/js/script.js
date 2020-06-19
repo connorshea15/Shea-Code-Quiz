67304412d509e93568589c1cf857e1721f6a2ef7
connorshea15 <connorshea15@gmail.com>
2020-06-19 13:27:04 -0700

    I am displaying the answers! but it may not be the best way to do it

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,11 @@ var questionCount = 0;
 // Let's actually make it an array of objects (Cool!!)
 // Let's create an object for our first question
 var questions = [
-    { question: "Commonly used data types do NOT include:", a1: "strings", 2: "booleans", 3: "alerts", 4: "numbers", correctAnswer: "alerts" },
+    { question: "Commonly used data types do NOT include:", a1: "strings", a2: "booleans", a3: "alerts", a4: "numbers", correctAnswer: "alerts" },
 
-    { question: "The condition in an If/Else statement is enclosed with:", 1: "brackets", 2: "parenthesis", 3: "quotes", 4: "apostophes", correctAnswer: "parenthesis" }
+    { question: "The condition in an If/Else statement is enclosed with:", a1: "brackets", a2: "parenthesis", a3: "quotes", a4: "apostophes", correctAnswer: "parenthesis" }
 ];
 
-console.log(questions[0].question);
-console.log(questions[1].question);
 
 // I need to create a function to display a specific question
 // I will pass "question" + questionId into the function, which has a 
@@ -28,11 +26,17 @@ var presentQuestion = function(currentQuestion) {
 
     // Create section for the answers
     // I need to look at my examples to see where they did the div thing
-    var answers = document.createElement("button");
-    answers.textContent = currentQuestion[questionCount].a1;
+    var answers = document.createElement("div");
+    answers.innerHTML = 
+        "<button>" + currentQuestion[questionCount].a1 + 
+        "</button></br><button>" + currentQuestion[questionCount].a2 + 
+        "</button></br><button>" + currentQuestion[questionCount].a3 +
+        "</button></br><button>" + currentQuestion[questionCount].a4 + 
+        "</button>";
     quizContentEl.append(answers);
 
-    questionCount++
+
+    // questionCount++
 };
 
 // I need a function to check the answer
