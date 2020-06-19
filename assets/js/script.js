a861b716e073c88d86c78b562abd48d1f2cefd38
connorshea15 <connorshea15@gmail.com>
2020-06-19 13:07:49 -0700

    I am displaying the question as a heading and answer 1 as a button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,18 @@
 // Create a variable to represent the main section
 var quizContentEl = document.querySelector("#quiz-content");
+var questionCount = 0;
 
+
+// Let's actually make it an array of objects (Cool!!)
 // Let's create an object for our first question
-var question1 = {
-    question: "Commonly used data types do NOT include:",
-    answer1: "strings",
-    answer2: "booleans",
-    answer3: "alerts",
-    answer4: "numbers",
-    correctAnswer: "alerts"
-};
+var questions = [
+    { question: "Commonly used data types do NOT include:", a1: "strings", 2: "booleans", 3: "alerts", 4: "numbers", correctAnswer: "alerts" },
+
+    { question: "The condition in an If/Else statement is enclosed with:", 1: "brackets", 2: "parenthesis", 3: "quotes", 4: "apostophes", correctAnswer: "parenthesis" }
+];
+
+console.log(questions[0].question);
+console.log(questions[1].question);
 
 // I need to create a function to display a specific question
 // I will pass "question" + questionId into the function, which has a 
@@ -17,17 +20,19 @@ var question1 = {
 
 var presentQuestion = function(currentQuestion) {
     // Create a variable for the question text
-    var question = document.createElement("h2");
+    var displayQuestion = document.createElement("h2");
     // Set the textContent of question variable to the question of my question object
-    question.textContent = currentQuestion.question;
+    displayQuestion.textContent = currentQuestion[questionCount].question;
     // Append the question to my page
-    quizContentEl.appendChild(question);
+    quizContentEl.appendChild(displayQuestion);
 
     // Create section for the answers
     // I need to look at my examples to see where they did the div thing
-    var answers = document.createElement("div");
-    quizContentEl.append()
+    var answers = document.createElement("button");
+    answers.textContent = currentQuestion[questionCount].a1;
+    quizContentEl.append(answers);
 
+    questionCount++
 };
 
 // I need a function to check the answer
@@ -35,7 +40,5 @@ var presentQuestion = function(currentQuestion) {
 
 
 
-var i = 1;
-var functionArgument = "question" + i;
-console.log(functionArgument);
-presentQuestion(functionArgument);
+
+presentQuestion(questions);
