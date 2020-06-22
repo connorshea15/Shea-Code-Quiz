aab604b633456cd9a98dc01fc1196a9b0f1c1a2b
connorshea15 <connorshea15@gmail.com>
2020-06-21 18:54:20 -0700

    Just a touch of CSS and some Readme action

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Shea-Code-Quiz
+# Shea Code Quizzer
+
+## Purpose
+To sharpen up the Javascript knowledge of aspiring coders everywhere
+
+# Built With
+* HTML
+* CSS
+* JAVASCRIPT
+
+## Mockup
+![](/assets/images/mockup.JPG)
+
+## Website
+
+
+## Contribution
+Made by Connor Shea
--- assets/js/script.js
+++ assets/js/script.js
@@ -45,7 +45,6 @@ var questions = [
 
 // function to start the quiz
 var startQuiz = function() {
-    // I probably need to dynamically create the header at the start of each quiz
     questionCount = 0;
     counter = 75;
     contentHeading.innerText = "Coding Quiz Challenge";
@@ -179,7 +178,6 @@ var clearHighScores = function() {
     startQuiz();
 };
 
-//getScores();
 startQuiz();
 document.getElementById("high-score-button").addEventListener("click", printHighScores);
 getScores();
